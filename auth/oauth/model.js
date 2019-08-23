330dd9a7ae2383527332022317dd6ed68eab3034
14gasher <asher.gunsay@gmail.com>
2019-08-23 16:51:13 -0600

    Added in a generateAuthorizationCode example

--- auth/oauth/model.js
+++ auth/oauth/model.js
@@ -1,5 +1,5 @@
 // See https://oauth2-server.readthedocs.io/en/latest/model/spec.html for what you can do with this
-
+const crypto = require('crypto')
 const db = { // Here is a fast overview of what your db model should look like
   authorizationCode: {
     authorizationCode: '', // A string that contains the code
@@ -25,13 +25,13 @@ const db = { // Here is a fast overview of what your db model should look like
 const DebugControl = require('../utilities/debug.js')
 
 module.exports = {
-  getClient: function(clientId, clientSecret) {
+  getClient: function (clientId, clientSecret) {
     // query db for details with client
     log({
       title: 'Get Client',
       parameters: [
-        {name: 'clientId', value: clientId},
-        {name: 'clientSecret', value: clientSecret},
+        { name: 'clientId', value: clientId },
+        { name: 'clientSecret', value: clientSecret },
       ]
     })
     db.client = { // Retrieved from the database
@@ -59,9 +59,9 @@ module.exports = {
     log({
       title: 'Save Token',
       parameters: [
-        {name: 'token', value: token},
-        {name: 'client', value: client},
-        {name: 'user', value: user},
+        { name: 'token', value: token },
+        { name: 'client', value: client },
+        { name: 'user', value: user },
       ],
     })
     db.token = {
@@ -80,10 +80,10 @@ module.exports = {
     log({
       title: 'Get Access Token',
       parameters: [
-        {name: 'token', value: token},
+        { name: 'token', value: token },
       ]
     })
-    if(!token || token === 'undefined') return false
+    if (!token || token === 'undefined') return false
     return new Promise(resolve => resolve(db.token))
   },
   getRefreshToken: token => {
@@ -91,10 +91,10 @@ module.exports = {
     log({
       title: 'Get Refresh Token',
       parameters: [
-        {name: 'token', value: token},
+        { name: 'token', value: token },
       ],
     })
-    DebugControl.log.variable({name: 'db.token', value: db.token})
+    DebugControl.log.variable({ name: 'db.token', value: db.token })
     return new Promise(resolve => resolve(db.token))
   },
   revokeToken: token => {
@@ -102,20 +102,54 @@ module.exports = {
     log({
       title: 'Revoke Token',
       parameters: [
-        {name: 'token', value: token},
+        { name: 'token', value: token },
       ]
     })
-    if(!token || token === 'undefined') return false
+    if (!token || token === 'undefined') return false
     return new Promise(resolve => resolve(true))
+  },
+  generateAuthorizationCode: (client, user, scope) => {
+    /* 
+    For this to work, you are going have to hack this a little bit:
+    1. navigate to the node_modules folder
+    2. find the oauth_server folder. (node_modules/express-oauth-server/node_modules/oauth2-server)
+    3. open lib/handlers/authorize-handler.js
+    4. Make the following change (around line 136):
+
+    AuthorizeHandler.prototype.generateAuthorizationCode = function (client, user, scope) {
+      if (this.model.generateAuthorizationCode) {
+        // Replace this
+        //return promisify(this.model.generateAuthorizationCode).call(this.model, client, user, scope);
+        // With this
+        return this.model.generateAuthorizationCode(client, user, scope)
+      }
+      return tokenUtil.generateRandomToken();
+    };
+    */
+
+    log({
+      title: 'Generate Authorization Code',
+      parameters: [
+        { name: 'client', value: client },
+        { name: 'user', value: user },
+      ],
+    })
+
+    const seed = crypto.randomBytes(256)
+    const code = crypto
+      .createHash('sha1')
+      .update(seed)
+      .digest('hex')
+    return code
   },
   saveAuthorizationCode: (code, client, user) => {
     /* This is where you store the access code data into the database */
     log({
       title: 'Save Authorization Code',
       parameters: [
-        {name: 'code', value: code},
-        {name: 'client', value: client},
-        {name: 'user', value: user},
+        { name: 'code', value: code },
+        { name: 'client', value: client },
+        { name: 'user', value: user },
       ],
     })
     db.authorizationCode = {
@@ -126,14 +160,14 @@ module.exports = {
     }
     return new Promise(resolve => resolve(Object.assign({
       redirectUri: `${code.redirectUri}`,
-    },db.authorizationCode)))
+    }, db.authorizationCode)))
   },
   getAuthorizationCode: authorizationCode => {
     /* this is where we fetch the stored data from the code */
     log({
       title: 'Get Authorization code',
       parameters: [
-        {name: 'authorizationCode', value: authorizationCode},
+        { name: 'authorizationCode', value: authorizationCode },
       ],
     })
     return new Promise(resolve => {
@@ -145,7 +179,7 @@ module.exports = {
     log({
       title: 'Revoke Authorization Code',
       parameters: [
-        {name: 'authorizationCode', value: authorizationCode},
+        { name: 'authorizationCode', value: authorizationCode },
       ],
     })
     db.authorizationCode = { // DB Delete in this in memory example :)
@@ -163,8 +197,8 @@ module.exports = {
     log({
       title: 'Verify Scope',
       parameters: [
-        {name: 'token', value: token},
-        {name: 'scope', value: scope},
+        { name: 'token', value: token },
+        { name: 'scope', value: scope },
       ],
     })
     const userHasAccess = true  // return true if this user / client combo has access to this resource
@@ -172,7 +206,7 @@ module.exports = {
   }
 }
 
-function log({title,parameters}) {
+function log({ title, parameters }) {
   DebugControl.log.functionName(title)
   DebugControl.log.parameters(parameters)
 }
